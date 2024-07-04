2b4545eb4282ea445dda66d8050c5eb0044d5e45
ayushsachan123 <ayushsachan.in@gmail.com>
2024-07-04 15:05:43 +0530

    add backend url

--- frontend/src/apicalls/index.js
+++ frontend/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-     baseURL: 'http://localhost:5000',
+     baseURL: 'https://online-quiz-maker.onrender.com',
     headers: {
        'authorization': `Bearer ${localStorage.getItem('token')}`
     }
